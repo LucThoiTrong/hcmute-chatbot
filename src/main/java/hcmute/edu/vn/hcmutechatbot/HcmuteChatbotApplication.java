64e33e85cce3271a625d444a0f93d657791615f3
Lục Thới Trọng <lucthoitrong@gmail.com>
2025-11-19 00:57:39 +0700

    set up environment

---
+++ src/main/java/hcmute/edu/vn/hcmutechatbot/HcmuteChatbotApplication.java
@@ -0,0 +1,11 @@
+package hcmute.edu.vn.hcmutechatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcmuteChatbotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HcmuteChatbotApplication.class, args);
+    }
+}
---
+++ src/test/java/hcmute/edu/vn/hcmutechatbot/HcmuteChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package hcmute.edu.vn.hcmutechatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcmuteChatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
