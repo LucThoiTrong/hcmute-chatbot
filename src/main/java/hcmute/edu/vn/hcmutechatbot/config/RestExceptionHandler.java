2d475bbc88c1327bedb74ed8b106d268259c0277
Lục Thới Trọng <lucthoitrong@gmail.com>
2025-11-23 15:39:06 +0700

    Build Global Exception Handler

---
+++ src/main/java/hcmute/edu/vn/hcmutechatbot/config/RestExceptionHandler.java
@@ -0,0 +1,40 @@
+package hcmute.edu.vn.hcmutechatbot.config;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    // Bắt ngoại lệ khi @Valid thất bại
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            @NonNull HttpHeaders headers,
+            @NonNull HttpStatusCode status,
+            @NonNull WebRequest request) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        // Trích xuất các lỗi validation từ exception
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        // Trả về 400 Bad Request với các lỗi chi tiết
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
